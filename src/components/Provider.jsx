f2a1651557c8a2a7baf418e574bafce1229d2db6
Faysal Ahmed <info.faysal.32@gmail.com>
2025-08-04 02:16:32 +0600

    starting an AI project which help in medical industry: complete navbar with signin and theme toggler design

---
+++ src/components/Provider.jsx
@@ -0,0 +1,15 @@
+// import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+// import { SessionProvider as CustomSessionProvider } from "@/lib/contexts/session-context";
+
+export default function Provider({ children }) {
+    return (
+        // <NextAuthSessionProvider>
+        //     <CustomSessionProvider>
+        <ThemeProvider attribute='class' defaultTheme="system" enableSystem disableTransitionOnChange>
+            {children}
+        </ThemeProvider>
+        //     </CustomSessionProvider>
+        // </NextAuthSessionProvider>
+    );
+}
